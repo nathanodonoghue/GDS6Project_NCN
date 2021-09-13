5475768885290214deec507b5e5fd66ac502a9a6
Nathan O'Donoghue <therealnathanod@gmail.com>
2021-09-14 04:17:49 +1000

    AI controller looks for behavior tree and blackboard

--- TestProjectGDS6/Source/TestProjectGDS6/EnemyAIController.cpp
+++ TestProjectGDS6/Source/TestProjectGDS6/EnemyAIController.cpp
@@ -4,25 +4,36 @@
 #include "EnemyAIController.h"
 
 #include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 void AEnemyAIController::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (AIBehavior != nullptr)
+	{
+		RunBehaviorTree(AIBehavior);
+
+		APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+	}
 }
 
 void AEnemyAIController::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
-	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
-	if (LineOfSightTo(PlayerPawn))
-	{
-		MoveToActor(PlayerPawn, AcceptanceRadius);
-		SetFocus(PlayerPawn);
-	}
-	else
-	{
-		ClearFocus(EAIFocusPriority::Gameplay);
-		StopMovement();
-	}
+	//APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+	//if (LineOfSightTo(PlayerPawn))
+	//{
+	//	MoveToActor(PlayerPawn, AcceptanceRadius);
+	//	SetFocus(PlayerPawn);
+	//}
+	//else
+	//{
+	//	ClearFocus(EAIFocusPriority::Gameplay);
+	//	StopMovement();
+	//}
 }
